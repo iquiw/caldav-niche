a6b7e92859973f6a2ffbdfce4b6cdbb3d1910897
Iku Iwasa <iku.iwasa@gmail.com>
2017-08-13 15:06:34 +0900

    Support calendar-description property in mkcalendar XML

--- test/xmldata-test.js
+++ test/xmldata-test.js
@@ -2,12 +2,23 @@ import test from 'ava';
 
 import { MkCalendarXml } from '../lib/xmldata.js';
 
-test(t => {
+test('mkcalendar XML without options', t => {
   let expected = '<?xml version="1.0" encoding="utf-8"?>'
       + '<C:mkcalendar xmlns="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">'
       + '<set><prop><resourcetype><collection/><C:calendar/></resourcetype>'
       + '<displayname>foo</displayname></prop></set>'
       + '</C:mkcalendar>';
   let actual = new MkCalendarXml('foo').toXml();
+  t.is(actual, expected);
+});
+
+test('mkcalendar XML with description', t => {
+  let expected = '<?xml version="1.0" encoding="utf-8"?>'
+      + '<C:mkcalendar xmlns="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">'
+      + '<set><prop><resourcetype><collection/><C:calendar/></resourcetype>'
+      + '<displayname>foo</displayname>'
+      + '<C:calendar-description>bar</C:calendar-description></prop></set>'
+      + '</C:mkcalendar>';
+  let actual = new MkCalendarXml('foo', { description: 'bar' }).toXml();
   t.is(actual, expected);
 });
