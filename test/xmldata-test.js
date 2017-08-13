0a8e878a102a58ac4e6dcb89cc5a727bc1b844a9
Iku Iwasa <iku.iwasa@gmail.com>
2017-08-13 13:58:44 +0900

    Fix expected mkcalendar XML data in test

--- test/xmldata-test.js
+++ test/xmldata-test.js
@@ -3,10 +3,11 @@ import test from 'ava';
 import { MkCalendarXml } from '../lib/xmldata.js';
 
 test(t => {
-  let xml = new MkCalendarXml('foo').toXml();
-  t.is(xml, '<?xml version="1.0" encoding="utf-8"?>'
-       + '<C:mkcalendar xmlns="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">'
-       + '<set><resourcetype><collection/><C:calendar/></resourcetype>'
-       + '<prop><displayname>foo</displayname></prop></set>'
-       + '</C:mkcalendar>');
+  let expected = '<?xml version="1.0" encoding="utf-8"?>'
+      + '<C:mkcalendar xmlns="DAV:" xmlns:C="urn:ietf:params:xml:ns:caldav">'
+      + '<set><prop><resourcetype><collection/><C:calendar/></resourcetype>'
+      + '<displayname>foo</displayname></prop></set>'
+      + '</C:mkcalendar>';
+  let actual = new MkCalendarXml('foo').toXml();
+  t.is(actual, expected);
 });
